2607ca290e9b687d272e046fb1d05aec714e5c75
Haikalbotz <97148397+Haikalbotz@users.noreply.github.com>
2022-01-05 13:24:52 +0700

    Add files via upload

---
+++ PdfMenu/menu.js
@@ -0,0 +1,133 @@
+const menunya = (prefix) => {
+	return`┌━━֍  〔 🪀*${botname}* 🪀〕
+│
+│ █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
+│ █░░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░░█
+│ █░░║║║╠─║─║─║║║║║╠─░░█
+│ █░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█
+│ █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
+│
+ Group Menu
+ヅ ${prefix}antilink
+ヅ ${prefix}welcome
+ヅ ${prefix}antivirtex
+ヅ ${prefix}group
+ヅ ${prefix} linkgroup
+ヅ ${prefix}promote
+ヅ ${prefix}demote
+ヅ ${prefix}add
+ヅ ${prefix}kick
+ヅ ${prefix}setpp
+ヅ ${prefix}setdesc
+ヅ ${prefix}setname
+ヅ ${prefix}hidetag
+|
+ Sticker Menu
+ヅ ${prefix}attp
+ヅ ${prefix}sticker
+ヅ ${prefix}tomp3
+ヅ ${prefix}tovideo
+|
+ Downloader Menu
+ヅ ${prefix}play
+ヅ ${prefix}ytsearch
+ヅ ${prefix}ytmp3
+ヅ ${prefix}ytmp4
+ヅ ${prefix}tiktoknowm
+ヅ ${prefix}tiktokmusic
+ヅ ${prefix}pinterest
+|
+  Islam Menu
+ヅ ${prefix}alquran
+ヅ ${prefix}asmaulhusna
+ヅ ${prefix}alquranaudio
+ヅ ${prefix}jadwalsholat
+ヅ ${prefix}kisahnabi
+ヅ ${prefix}listsurah
+|    
+  Anime Menu
+ヅ ${prefix}character
+ヅ ${prefix}manga
+ヅ ${prefix}anime
+ヅ ${prefix}kusonimesearch
+ヅ ${prefix}otakudesusearch
+ヅ ${prefix}nhentaisearch
+ヅ ${prefix}nekopoisearch
+|   
+Information Menu
+ヅ ${prefix}kbbi
+ヅ ${prefix}infogempa
+ヅ ${prefix}covidindo
+ヅ ${prefix}covidglobal
+ヅ ${prefix}jadwaltv
+ヅ ${prefix}translate
+ヅ ${prefix}cuaca
+ヅ ${prefix}wikipedia
+ヅ ${prefix}jarak
+|
+Search Menu
+ヅ ${prefix}gimage
+ヅ ${prefix}wallpapersearch
+ヅ ${prefix}playstore
+ヅ ${prefix}shopee
+ヅ ${prefix}google
+|
+Primbon Menu
+ヅ ${prefix}tebakumur
+ヅ ${prefix}artinama
+ヅ ${prefix}jodoh
+ヅ ${prefix}jadian
+|
+Asupan Menu
+ヅ ${prefix}asupan
+ヅ ${prefix}asupancecan
+ヅ ${prefix}asupanhillllljaber
+ヅ ${prefix}asupansantuy
+ヅ ${prefix}asupanukhti
+ヅ ${prefix}asupanbocil
+ヅ ${prefix}asupanghea
+ヅ ${prefix}asupanrika
+|
+Game Menu
+ヅ ${prefix}slot
+ヅ ${prefix}truth
+ヅ ${prefix}dare
+ヅ ${prefix}tembak
+ヅ ${prefix}tebakjenaka
+ヅ ${prefix}tebaklirik
+ヅ ${prefix}tebaktebakan
+ヅ ${prefix}caklontong
+|
+ sound menu
+ヅ ${prefix}sound1
+ヅ ${prefix}sound2
+ヅ ${prefix}sound3
+ヅ ${prefix}sound4
+ヅ ${prefix}sound5
+ヅ ${prefix}sound6
+ヅ ${prefix}sound7
+ヅ ${prefix}sound8
+ヅ ${prefix}sound9
+ヅ ${prefix}sound10
+ヅ ${prefix}sound11
+ヅ ${prefix}sound12
+ヅ ${prefix}sound13
+ヅ ${prefix}sound14
+ヅ ${prefix}sound15
+|
+ Maker Menu
+ヅ ${prefix}neon
+ヅ${prefix}glitch
+ヅ ${prefix}thunder
+ヅ ${prefix}tahta
+ヅ ${prefix}glow
+|
+Owner Menu
+ヅ ${prefix}owner
+ヅ ${prefix}sewabot
+ヅ ${prefix}bc
+ヅ ${prefix}report
+⏐
+└━━֍  〔 🪀 *${botname}* 🪀〕`
+}
+exports.menunya = menunya
